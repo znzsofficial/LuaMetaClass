6931ddcc844441b0b9aa5a3ed2a3dff4aeaf4f0f
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2023-08-31 21:49:19 +0800

    Update dump.lua

--- dump.lua
+++ dump.lua
@@ -1,42 +1,53 @@
+local function processValue(value)
+  if type(value) == "function" then
+    -- 函数类型处理
+    local info = debug.getinfo(value, "Slnuf")
+    local infoStr = ""
+    for key, value in pairs(info) do
+      if key ~= "func" and value ~= "" then
+        infoStr = infoStr .. indent .. "" .. key .. ": " .. tostring(value) .. "\n"
+      end
+    end
+    return "<function>" .. tostring(value) .. "\n" .. infoStr
+  elseif type(value) == "table" then
+    -- 表类型处理
+    local str = dump(value, seen, indent .. " ")
+    if getmetatable(value) then
+      str = str .. "\n" .. indent .. "<metatable> =" .. dump(getmetatable(value), seen, indent .. " ")
+    end
+    return "<table>" .. str
+  elseif type(value) == "string" then
+    -- 字符串类型处理
+    return "<string> \"" .. tostring(value) .. "\""
+  else
+    -- 其他类型直接返回
+    return "<" .. type(value) .. ">" .. tostring(value)
+  end
+end
+
 ---@param t table<any>
 ---@param seen table
 ---@param indent string
-return function (t, seen, indent)
-    seen = seen or {}
-    indent = indent or ""
-    if type (t) ~= "table" then
-      return tostring (t)
-    end
-  
-    if seen [t] then
-      return "<circular reference>"
-    end
-    seen [t] = true
-  
-    local res = {}
-    for k, v in pairs (t) do
-      local k_str = "<" .. type (k) .. "> [" .. dump (k, seen, indent .. "") .."]"
-      local v_str
-      if type (v) == "function" then
-        -- 如果是函数，同时输出 debug 信息
-        local info = debug.getinfo (v, "Slnuf")
-        v_str = tostring (v) .. "\n"
-        for key, value in pairs (info) do
-          v_str = v_str .. indent .. "" .. key ..": ".. tostring (value) .."\n"
-        end
-       elseif type (v) == "table" then
-        -- 如果是表，则递归处理子表
-        v_str = dump (v, seen, indent .. " ")
-        if getmetatable (v) then
-          -- 如果有元表，则额外输出元表
-          v_str = v_str .. "\n" .. indent .. "<metatable> =" .. dump (getmetatable (v), seen, indent .. " ")
-        end
-        v_str = "<" .. type (v) .. ">" .. v_str
-       else
-        -- 否则直接输出值
-        v_str = "<" .. type (v) .. ">" .. tostring (v)
-      end
-      table.insert (res, indent .. k_str .. "=" .. v_str)
-    end
-    return "{\n" .. table.concat (res, ",\n") .. "\n".. indent .. "}"
+return function(t, seen, indent)
+  seen = seen or {}
+  indent = indent or ""
+
+
+  if type(t) ~= "table" then
+    return tostring(t)
   end
+
+  if seen[t] then
+    return "<circular reference>"
+  end
+  seen[t] = true
+
+  local res = {}
+  for k, v in pairs(t) do
+    local kStr = processValue(k)
+    local vStr = processValue(v)
+    table.insert(res, indent .. kStr .. "=" .. vStr)
+  end
+
+  return "{\n" .. table.concat(res, ",\n") .. "\n" .. indent .. "}"
+end
